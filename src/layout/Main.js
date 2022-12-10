540b7d19dc29fad81cb082aea7a2265aee71c04c
Faisal Karim <faisalchy64@gmail.com>
2022-12-11 02:36:27 +0600

    i have created a footer component

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 function Main() {
@@ -6,6 +7,7 @@ function Main() {
         <>
             <Navbar />
             <Outlet />
+            <Footer />
         </>
     );
 }
